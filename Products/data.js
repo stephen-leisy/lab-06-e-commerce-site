ccdfcb8ca4b3f76e6cc13aeea02d556ed15d5563
stephen-leisy <stleisy@gmail.com>
2021-01-19 13:59:23 -0800

    created static html item.

---
+++ Products/data.js
@@ -0,0 +1,10 @@
+export const albums = [
+    {
+        id: 1,
+        name: 'Sun Ra Space Is The Place',
+        image: 'sun-ra.png',
+        description: `an excellent introduction to Sun Ra's vast and free-form jazz catalog`,
+        category: 'jazz',
+        price: 20,
+    },
+];
