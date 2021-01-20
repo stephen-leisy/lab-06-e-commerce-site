f50f1a7ff217edd2ff036e79d6c72cd8a2fc753a
stephen-leisy <stleisy@gmail.com>
2021-01-19 17:25:58 -0800

    forgot to commit, but added render function with test and added loop for page items.

--- Products/app.js
+++ Products/app.js
@@ -1,5 +1,14 @@
 // import functions and grab DOM elements
-
+import { albums } from './album-data.js';
+import { renderAlbums } from './render-albums.js';
 // initialize state
-
+console.log(albums);
 // set event listeners to update state and DOM
+const list = document.getElementById('list');
+
+for (const album of albums) {
+
+    const albumElement = renderAlbums(album);
+
+    list.append(albumElement);
+}
--- Products/render-albums.js
+++ Products/render-albums.js
@@ -1,3 +1,38 @@
-export function renderAlbums() {
+export function renderAlbums(album) {
 
+    const li = document.createElement('li');
+    li.classList.add('album-item');
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('album-name');
+    h2.textContent = album.name;
+    li.append(h2);
+
+    const img = document.createElement('img');
+    img.classList.add('album-cover');
+    img.src = `../assets/${album.image}`;
+    li.append(img);
+
+
+    const pDescription = document.createElement('p');
+    pDescription.classList.add('description');
+    pDescription.textContent = album.description;
+    li.append(pDescription);
+
+    const pCategory = document.createElement('p');
+    pCategory.classList.add('category');
+    pCategory.textContent = album.category;
+    li.append(pCategory);
+
+    const pPrice = document.createElement('p');
+    pPrice.classList.add('price');
+    pPrice.textContent = album.price;
+    li.append(pPrice);
+
+    const addToCartButton = document.createElement('button');
+    addToCartButton.classList.add('add-to-cart');
+    addToCartButton.textContent = `Add To Cart`;
+    li.append(addToCartButton);
+
+    return li;
 }
