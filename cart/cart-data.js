4d8f3e0da5cca44f0d6f1c7ea09f8afa4ed6f460
stephen-leisy <stleisy@gmail.com>
2021-01-20 14:35:24 -0800

    Step 2 added dummy cart array and exported it.

---
+++ cart/cart-data.js
@@ -0,0 +1,15 @@
+export const cartItems = [
+    {
+        id: 3,
+        quantity: 2
+
+    },
+    {
+        id: 7,
+        quantity: 1
+    },
+    {
+        id: 5,
+        quantity: 3
+    },
+];
